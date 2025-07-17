3e3f82fefa7ddada78d6836e3749763a0ea2c304
RAHUL MALLIK <rmallik191242@bscse.uiu.ac.bd>
2025-07-17 05:43:49 -0600

    changed the metadata , and import headding to the home

--- src/app/page.js
+++ src/app/page.js
@@ -1,12 +1,13 @@
 /** @format */
 
+import Heading from "@/components/Heading";
 import Image from "next/image";
 
 export default function Home() {
   return (
     <div className="flex min-h-screen flex-col items-center justify-between p-24">
       <h1>Hello next hero</h1>
-      <button>Call me</button>
+      <Heading />
     </div>
   );
 }

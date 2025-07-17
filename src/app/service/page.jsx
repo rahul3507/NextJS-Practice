b7bc1c84cd60d367be051cdf7a5686de0966dd30
RAHUL MALLIK <rmallik191242@bscse.uiu.ac.bd>
2025-07-17 05:51:53 -0600

    service page created

---
+++ src/app/service/page.jsx
@@ -0,0 +1,13 @@
+/** @format */
+
+import React from "react";
+
+const ServicePage = () => {
+  return (
+    <div>
+      <h1>this is service page</h1>
+    </div>
+  );
+};
+
+export default ServicePage;

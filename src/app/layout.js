0c036266e83f4006acfaa72f36359ed4e019e294
RAHUL MALLIK <rmallik191242@bscse.uiu.ac.bd>
2025-07-18 22:24:39 -0600

    updated navbar and called into the layout

--- src/app/layout.js
+++ src/app/layout.js
@@ -2,6 +2,7 @@
 
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
+import Navbar from "@/components/Navbar";
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
@@ -24,6 +25,7 @@ export default function RootLayout({ children }) {
       <body
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
       >
+        <Navbar />
         <div className="h-screen">{children}</div>
       </body>
     </html>
--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -4,13 +4,14 @@ import React from "react";
 
 const Navbar = () => {
   return (
-    <nav className="bg-amber-400 px-6 py-4">
-      <h6>LOGO</h6>
-      <ul>
-        <li>About</li>
-        <li>Service</li>
-        <li>Contact</li>
+    <nav className="bg-amber-400 px-6 py-4 flex justify-between text-black">
+      <h6 className="cursor-pointer">LOGO</h6>
+      <ul className="flex gap-6">
+        <li className="cursor-pointer">About</li>
+        <li className="cursor-pointer">Service</li>
+        <li className="cursor-pointer">Contact</li>
       </ul>
+      <button className="cursor-pointer">Log in</button>
     </nav>
   );
 };

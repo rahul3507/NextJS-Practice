b2accbe51402f8ff695975c2bbc37e209a8b29de
RAHUL MALLIK <rmallik191242@bscse.uiu.ac.bd>
2025-07-18 22:43:07 -0600

    link added to home and about button

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,5 +1,6 @@
 /** @format */
 
+import Link from "next/link";
 import React from "react";
 
 const Navbar = () => {
@@ -7,7 +8,12 @@ const Navbar = () => {
     <nav className="bg-amber-400 px-6 py-4 flex justify-between text-black">
       <h6 className="cursor-pointer">LOGO</h6>
       <ul className="flex gap-6">
-        <li className="cursor-pointer">About</li>
+        <li className="cursor-pointer">
+          <Link href={"/"}>Home</Link>
+        </li>
+        <li className="cursor-pointer">
+          <Link href={"/about"}>About</Link>
+        </li>
         <li className="cursor-pointer">Service</li>
         <li className="cursor-pointer">Contact</li>
       </ul>

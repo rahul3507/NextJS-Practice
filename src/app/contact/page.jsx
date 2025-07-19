f93d2bada81536a064dc2537ae735c41c5274944
RAHUL MALLIK <rmallik191242@bscse.uiu.ac.bd>
2025-07-18 22:57:03 -0600

    contact page created

---
+++ src/app/contact/page.jsx
@@ -0,0 +1,13 @@
+/** @format */
+
+import React from "react";
+
+const Contact = () => {
+  return (
+    <div>
+      <h1>this is the conact page</h1>
+    </div>
+  );
+};
+
+export default Contact;

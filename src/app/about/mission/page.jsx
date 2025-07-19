a6ddaec34c02c880ea67eff197a40a8b583e76ca
RAHUL MALLIK <rmallik191242@bscse.uiu.ac.bd>
2025-07-19 05:00:40 -0600

    created to nasted page into the about

---
+++ src/app/about/mission/page.jsx
@@ -0,0 +1,13 @@
+/** @format */
+
+import React from "react";
+
+const Mission = () => {
+  return (
+    <div>
+      <h1>Mission</h1>
+    </div>
+  );
+};
+
+export default Mission;
--- src/components/AboutContents.jsx
+++ src/components/AboutContents.jsx
@@ -1,11 +1,15 @@
 /** @format */
 
+import Link from "next/link";
 import React from "react";
 
 const AboutContents = () => {
   return (
-    <div>
-      <h1>AboutContents</h1>
+    <div className="text-center justify-center items-center gap-4">
+      <div className="space-x-6">
+        <Link href="/about/history">History</Link>
+        <Link href="/about/mission">Mission</Link>
+      </div>
     </div>
   );
 };

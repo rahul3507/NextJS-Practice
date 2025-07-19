06307a18646044acd9fa82f17f16d011e41821ff
RAHUL MALLIK <rmallik191242@bscse.uiu.ac.bd>
2025-07-18 22:17:24 -0600

    navbar component created

---
+++ src/components/Navbar.jsx
@@ -0,0 +1,18 @@
+/** @format */
+
+import React from "react";
+
+const Navbar = () => {
+  return (
+    <nav className="bg-amber-400 px-6 py-4">
+      <h6>LOGO</h6>
+      <ul>
+        <li>About</li>
+        <li>Service</li>
+        <li>Contact</li>
+      </ul>
+    </nav>
+  );
+};
+
+export default Navbar;

2c4227b722ed63d8791104ebd1c82ff737cf53f3
RAHUL MALLIK <rmallik191242@bscse.uiu.ac.bd>
2025-07-18 22:46:30 -0600

    get a links variable

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -4,6 +4,20 @@ import Link from "next/link";
 import React from "react";
 
 const Navbar = () => {
+  const links = [
+    {
+      title: "home",
+      path: "/",
+    },
+    {
+      title: "About",
+      path: "/about",
+    },
+    {
+      title: "Contact",
+      path: "/contact",
+    },
+  ];
   return (
     <nav className="bg-amber-400 px-6 py-4 flex justify-between text-black">
       <h6 className="cursor-pointer">LOGO</h6>

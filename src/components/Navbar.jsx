011cf8e06ca53abe228be0f4018e8991a99ab774
RAHUL MALLIK <rmallik191242@bscse.uiu.ac.bd>
2025-07-18 22:54:04 -0600

    map the navbar links

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -22,14 +22,11 @@ const Navbar = () => {
     <nav className="bg-amber-400 px-6 py-4 flex justify-between text-black">
       <h6 className="cursor-pointer">LOGO</h6>
       <ul className="flex gap-6">
-        <li className="cursor-pointer">
-          <Link href={"/"}>Home</Link>
-        </li>
-        <li className="cursor-pointer">
-          <Link href={"/about"}>About</Link>
-        </li>
-        <li className="cursor-pointer">Service</li>
-        <li className="cursor-pointer">Contact</li>
+        {links?.map((link) => (
+          <Link key={link.path} href={link.path}>
+            {link.title}
+          </Link>
+        ))}
       </ul>
       <button className="cursor-pointer">Log in</button>
     </nav>

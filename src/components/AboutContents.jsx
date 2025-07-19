555f96b3480e5c1b5d839840ea7241f67dbaafc8
RAHUL MALLIK <rmallik191242@bscse.uiu.ac.bd>
2025-07-18 22:07:32 -0600

    aboutcontents component created

---
+++ src/components/AboutContents.jsx
@@ -0,0 +1,13 @@
+/** @format */
+
+import React from "react";
+
+const AboutContents = () => {
+  return (
+    <div>
+      <h1>AboutContents</h1>
+    </div>
+  );
+};
+
+export default AboutContents;

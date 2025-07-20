cf2b02fcc91934ff27ce1a9a6d3de361dd528a77
RAHUL MALLIK <rmallik191242@bscse.uiu.ac.bd>
2025-07-20 00:40:10 -0600

    error page created

---
+++ src/app/error.jsx
@@ -0,0 +1,13 @@
+/** @format */
+"use client";
+import React from "react";
+
+const error = () => {
+  return (
+    <div className="text-center items-center justify-center m-auto text-red-600 text-5xl">
+      <h1 className="mt-32 h-64">404</h1>
+    </div>
+  );
+};
+
+export default error;

5782bc76400727ad70b2cb936d84b0fe51ace076
RAHUL MALLIK <rmallik191242@bscse.uiu.ac.bd>
2025-07-20 23:20:30 -0600

    generatemetadata into id

--- src/app/posts/[id]/page.jsx
+++ src/app/posts/[id]/page.jsx
@@ -2,6 +2,15 @@
 
 import React from "react";
 
+export const generateMetadata = async ({ params }) => {
+  return {
+    title: "Post Details",
+
+    description: "This is from post details",
+    keywords: ["about", "about page"],
+  };
+};
+
 const PostDetailsPage = async ({ params }) => {
   const { id } = await params;
   console.log(id);

be99dff09362a9b2ef5de18fe9f18d7dd2f3fcb4
RAHUL MALLIK <rmallik191242@bscse.uiu.ac.bd>
2025-07-21 00:49:00 -0600

    time import to the about page

--- src/app/about/page.jsx
+++ src/app/about/page.jsx
@@ -9,10 +9,19 @@ export const metadata = {
   keywords: ["about", "about page"],
 };
 
-const AboutPage = () => {
+const getTime = async () => {
+  const res = await fetch("http://localhost:3000/time");
+  const data = await res.json();
+  return data.currentTime;
+};
+
+const AboutPage = async () => {
+  const currentTime = await getTime();
   return (
     <div>
       <h1 className="text-red-500">this is about page</h1>
+
+      <h3>Current Time : {currentTime}</h3>
       <AboutContents />
     </div>
   );
--- src/app/time/route.js
+++ src/app/time/route.js
@@ -1,7 +1,5 @@
 /** @format */
 
-export const dynamic = "force-dynamic";
-
 export async function Get() {
   return Response.json({
     currentTime: new Date().toLocaleDateString(),
